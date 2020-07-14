8de6ec3ab8bbb486a7ff2e871b4460882028cdbd
James <freemanpolys@gmail.com>
2020-07-14 08:06:01 +0000

    Update multi-stage

--- image-docker/09-dockerfile-multi-stage/build-run.sh
+++ image-docker/09-dockerfile-multi-stage/build-run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#Image basique
 docker build -t myimage:1.0.9 .
 docker rm -f myweb
 docker run --name myweb -p 80:80 -d myimage:1.0.9
+
+#Optimisation Manuelle
+npm run build -- --outputPath=./dist/out --configuration production
+docker build -t myimage:1.0.9-manual-stage  -f DockerfileManual .
+docker rm -f myweb
+docker run --name myweb -p 80:80 -d myimage:1.0.9-manual-stage
+
+#Optimisation avec Multi Stage
+docker build -t myimage:1.0.9-multi-stage  -f DockerfileMultiStage .
+docker rm -f myweb
+docker run --name myweb -p 80:80 -d myimage:1.0.9-multi-stage
